7d0ec48df827dc2bf465ba9d016ae109c33019c1
Jordan Holt <66709081+jordanholtdev@users.noreply.github.com>
2022-12-29 02:43:25 +0000

    Update API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyAFGfQVycOy2nc9CZdwA4szV4NC9wpsuK4";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
